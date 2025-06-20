7430f3ba4996d562f5f27d10958e4437012eab22
lemoinep <patrick.lemoine@cemosis.fr>
2025-06-20 15:11:50 +0200

    First

---
+++ GPS_Yocto_Basis.py
@@ -0,0 +1,48 @@
+
+
+import os, sys
+from yocto_api import *
+from yocto_gps import *
+
+def usage():
+    scriptname = os.path.basename(sys.argv[0])
+    print("Usage:")
+    print(scriptname + ' <serial_number>')
+    print(scriptname + ' <logical_name>')
+    print(scriptname + ' any')
+    sys.exit()
+
+def die(msg):
+    sys.exit(msg + ' (check USB cable)')
+
+if len(sys.argv) < 2:
+    usage()
+target = sys.argv[1]
+
+# Setup the API to use local USB devices
+errmsg = YRefParam()
+if YAPI.RegisterHub("usb", errmsg) != YAPI.SUCCESS:
+    sys.exit("init error: " + errmsg.value)
+
+if target.lower() == 'any':
+    # Retrieve any GPS
+    gps = YGps.FirstGps()
+    if gps is None:
+        die('No module connected')
+    m = gps.get_module()
+    target = m.get_serialNumber()
+else:
+    gps = YGps.FindGps(target + '.gps')
+
+if not gps.isOnline():
+    die('device not connected')
+
+print("Using device:", target)
+while gps.isOnline():
+    if gps.get_isFixed() != YGps.ISFIXED_TRUE:
+        print("Fixing...")
+    else:
+        print(gps.get_latitude() + " " + gps.get_longitude())
+    YAPI.Sleep(1000)
+    
+YAPI.FreeAPI()
